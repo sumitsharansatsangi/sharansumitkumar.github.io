18064533c6dc3cdb96ef073cd21c1840bf5a9189
sumitsharansatsangi <sharansumitkumar@gmail.com>
2024-09-28 16:57:37 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('theme-toggle').addEventListener('click', () => {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', newTheme);
+});
