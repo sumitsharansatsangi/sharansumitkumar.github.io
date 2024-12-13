8a71d39f85d598650746a487c9b1248f3202b739
sumitsharansatsangi <sharansumitkumar@gmail.com>
2024-12-13 21:54:08 +0530

    theme change icon added

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const themeToggleButton = document.getElementById('theme-toggle');
-    themeToggleButton.addEventListener('click', function() {
+    const toggleIcon = document.getElementById('toggle-icon');
+    themeToggleButton.addEventListener('click', function () {
         document.body.classList.toggle('dark');
+        if (document.body.classList.contains('dark')) {
+            toggleIcon.textContent = '🌙';
+        }
+        else {
+            toggleIcon.textContent = '🌞';
+        }
         saveThemePreference();
     });
 
